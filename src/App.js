b86a2d3e3e5ed17b25514e450779c582a11f070f
SusmithaLakshmiKarri26 <147433621+SusmithaLakshmiKarri26@users.noreply.github.com>
2026-02-06 10:08:25 +0530

    Fix build issues and update portfolio

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Achievements from "./components/Achievements";
-import Contact from "./components/Contact";
 import Certifications from "./components/Certifications";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
